578cc4f1c05193425882d31bad936d6e3623ba83
James Kong <jkongie@gmail.com>
2013-05-10 03:41:00 +0800

    Fix specs

--- spec/classes/astrill_spec.rb
+++ spec/classes/astrill_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe 'astrill' do
 
   it { should contain_class('astrill') }
-  it { should contain_package('Astrill').with_provider('appdmg') }
+  it { should contain_package('Astrill').with_provider('pkgdmg') }
   it { should contain_package('Astrill').with_source('http://download.astrill.com/astrill-setup-mac.dmg') }
 
 end
