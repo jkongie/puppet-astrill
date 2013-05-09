8849c5a51316ffc94cbaddaf6502caeb2b4581ae
James Kong <jkongie@gmail.com>
2013-05-10 02:40:45 +0800

    Fix naming error produced from a copy and paste

--- spec/classes/astrill_spec.rb
+++ spec/classes/astrill_spec.rb
@@ -4,6 +4,6 @@ describe 'astrill' do
 
   it { should contain_class('astrill') }
   it { should contain_package('Astrill').with_provider('appdmg') }
-  it { should contain_package('Adium').with_source('https://www.astrill.com/downloads/astrill-setup-mac.dmg?mirror=usa') }
+  it { should contain_package('Astrill').with_source('https://www.astrill.com/downloads/astrill-setup-mac.dmg?mirror=usa') }
 
 end
